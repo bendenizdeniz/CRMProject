e5cba60fd6a25f82e3dc358efdd6304a1d7e4a9b
BaybarsCan <cankaanbaybars@gmail.com>
2021-10-02 13:31:46 +0300

    Refactoring verify password

---
+++ CRMProject/sendEmail.cs
@@ -0,0 +1,51 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace CRMProject
+{
+    public class sendEmail
+    {
+        static public void TryEmail(string email, string validationCodeMessage)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("CRM System", "crmsystem99@gmail.com"));
+            message.To.Add(MailboxAddress.Parse(email));
+            message.Subject = "Forgot Password Code";
+
+            var builder = new BodyBuilder();
+
+            builder.TextBody = @validationCodeMessage;
+
+            message.Body = builder.ToMessageBody();
+
+            string emailAddress = "crmsystem99@gmail.com";
+            string password = "dbcrmsystem";
+
+            MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient();
+
+            try
+            {
+                client.CheckCertificateRevocation = false;
+                client.Connect("smtp.gmail.com", 465, true);
+                client.Authenticate(emailAddress, password);
+                client.Send(message);
+
+                Console.WriteLine("Email Sent!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+    }
+}
--- Entity/User.cs
+++ Entity/User.cs
@@ -16,6 +16,8 @@ namespace Entity
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
+        public bool? Status { get; set; }
 
         public virtual ICollection<Customer> Customer { get; set; }
     }
