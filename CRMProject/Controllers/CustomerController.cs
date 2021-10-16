cab2cbc8c651d526dc597950d12c9cba3b5ccd60
bendenizdeniz <denizcanaltun0@gmail.com>
2021-10-16 22:30:34 +0300

    Refactoring DateTime

---
+++ CRMProject/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Interfaces;
+using CRMProject.Models;
+using Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService customerServices;
+        private readonly IUserService userServices;
+
+        public CustomerController(ICustomerService _customerService, IUserService _userService)
+        {
+            customerServices = _customerService;
+            userServices = _userService;
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateCustomer()
+        {
+            CustomerUserModel model = new();
+            model.User = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("SessionUser"));
+            model.UserList = userServices.GetAllUser();
+
+            return View(model: model);
+        }
+
+        [HttpPost]
+        public bool CreateCustomer(Customer customer)
+        {
+            Customer _customer = customerServices.GetCustomerById(customer.Id);
+            if (_customer is null)
+            {
+                customerServices.CreateCustomer(customer);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CRMProject/ViewComponents/AppointmentListByUserViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Business.Interfaces;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMProject.ViewComponents
+{
+    public class AppointmentListByUserViewComponent : ViewComponent
+    {
+        private readonly ICustomerService customerService;
+        private readonly IUserService userService;
+        private readonly IAppointmentService appointmentService;
+
+        public AppointmentListByUserViewComponent(ICustomerService _customerService, IUserService _userService, IAppointmentService _appointmentService)
+        {
+            userService = _userService;
+            customerService = _customerService;
+            appointmentService = _appointmentService;
+        }
+
+        public IViewComponentResult Invoke(List<Appointment> list)
+        {
+            if (list.Count > 0)
+            {
+                foreach (var item in list)
+                {
+                    item.Customer = customerService.GetCustomerById((int)item.CustomerId);
+                    item.User = userService.GetUserById((int)item.UserId);
+                }
+
+                return View(model: list);
+            }
+
+            return View();
+        }
+    }
+}
